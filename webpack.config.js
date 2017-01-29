888f584077dc14b6668f27f1ad0494787e2bd28e
Timothy Lee <timothy.d.lee@live.com>
2017-01-29 14:29:36 -0600

    continuing to adjust webpack.config.js to attempt to fix deployment issue

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,20 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV' : JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     loaders: [{
       exclude: /node_modules/,
